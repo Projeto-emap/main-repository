8980cbf34e250e38c485201920aff192b58469f9
Rocha06100 <felipe.rocha@sptech.school>
2024-09-10 18:07:42 -0300

    Subindo o log corrigido

--- Logemap/src/main/java/log.java
+++ Logemap/src/main/java/log.java
@@ -9,19 +9,34 @@ import java.time.format.DateTimeFormatter;
 
                 try {
                     System.out.println(dataFormatada + " Iniciando o sistema");
+                    agora = LocalDateTime.now();
+                    formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    dataFormatada = agora.format(formatador);
                     Thread.sleep(2000);
 
                     System.out.println(dataFormatada + " Identificando oportunidades");
+                    agora = LocalDateTime.now();
+                    formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    dataFormatada = agora.format(formatador);
                     Thread.sleep(2000);
 
                     System.out.println(dataFormatada + " Realizando consulta no banco de dados");
-                    Thread.sleep(2000); 
+                    agora = LocalDateTime.now();
+                    formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    dataFormatada = agora.format(formatador);
+                    Thread.sleep(2000);
 
 
                     System.out.println(dataFormatada + " Cadastrando um novo eletroposto");
+                    agora = LocalDateTime.now();
+                    formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    dataFormatada = agora.format(formatador);
                     Thread.sleep(2000);
 
                     System.out.println(dataFormatada + " Atualizando o sistema");
+                    agora = LocalDateTime.now();
+                    formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    dataFormatada = agora.format(formatador);
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     System.out.println("A execução foi interrompida.");
