7d642f73898f7ed9c32796a22582786c27b4f0cb
nathannolacio <nathannolacio04@gmail.com>
2024-12-09 22:21:31 -0300

    Funcao de cadastrar empresa e usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,23 +10,44 @@ function logar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(cnpj, nomeEmpresa, estado, cep, nome, cpf, email, celular, senha) {
+async function cadastrar(cnpj, nomeEmpresa, estado, cep, nome, cpf, email, celular, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", cnpj, nomeEmpresa, estado, cep, nome, cpf, email, celular, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
         INSERT INTO empresa (razaoSocial, cnpj, estado, cep) VALUES ('${nomeEmpresa}', '${cnpj}', '${estado}', '${cep}');
-        `;
-    var instrucaoSql2 = `
-    INSERT INTO usuario (nome, cpf, email, numeroCelular, senha) VALUES ('${nome}', '${cpf}', '${email}', '${celular}', '${senha}');    
     `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    console.log("Executando a instrução SQL: \n" + instrucaoSql2);
+    console.log("Executando a instrução SQL para empresa: \n" + instrucaoSql);
 
-    var resultado = Promise.all([database.executar(instrucaoSql), database.executar(instrucaoSql2)])
-    return resultado;
+    try {
+        const resultadoEmpresa = await database.executar(instrucaoSql);
+        const empresaId = resultadoEmpresa.insertId;
+
+        console.log("Empresa cadastrada com sucesso! ID da empresa:", empresaId);
+
+        var instrucaoSql2 = `
+            INSERT INTO usuario (nome, cpf, email, numeroCelular, senha, fkEmpresa) VALUES ('${nome}', '${cpf}', '${email}', '${celular}', '${senha}', ${empresaId});    
+        `;
+
+        console.log("Executando a instrução SQL para usuário: \n" + instrucaoSql2);
+
+        const resultadoUsuario = await database.executar(instrucaoSql2);
+
+        console.log("Usuário cadastrado com sucesso!");
+
+        return { sucesso: true, empresaId, usuarioId: resultadoUsuario.insertId };
+    } catch (erro) {
+        console.error("Erro ao cadastrar empresa e usuário: ", erro);
+        throw erro;
+    }
+
+    // console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    // console.log("Executando a instrução SQL: \n" + instrucaoSql2);
+
+    // var resultado = Promise.all([database.executar(instrucaoSql), database.executar(instrucaoSql2)])
+   
 }
 
 function deletar(idUsuario) {
